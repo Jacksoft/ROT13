371c83938e2bfe3492ba8dc4bc651d592f99d153
Jacksoft <jacksoft87@gmail.com>
2019-05-01 21:13:10 +0200

    First Release
    ROT13 Cipher/Decipher made in C#

---
+++ ROT13/ROT13.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ROT13 {
+    partial class ROTForm {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(11, 26);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(350, 135);
+            this.textBox_input.TabIndex = 0;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Location = new System.Drawing.Point(11, 197);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ReadOnly = true;
+            this.textBox_output.Size = new System.Drawing.Size(350, 135);
+            this.textBox_output.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output:";
+            // 
+            // ROTForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 341);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_output);
+            this.Controls.Add(this.textBox_input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ROTForm";
+            this.Text = "ROT13";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_output;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ROT13/ROT13.cs
@@ -0,0 +1,75 @@
+﻿/* ROT13 Cipher/Decipher by Jacksoft (c) 2019
+ * This is my C# version of ROT13 Cipher/Decipher
+ * Done just for fun.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROT13 {
+    public partial class ROTForm : Form {
+        public ROTForm() {
+            InitializeComponent();
+        }
+
+        //Array of alphabet in both upper and lower case.
+        string alphabetl = "abcdefghijklmnopqrstuvwxyz";
+        string alphabetu = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int shiftnum = 13; //ROT value
+        int alphabetlen = 26; //Number of alphabeth chars
+
+        public string shifter(string inputtxt) {
+            string outputtext = "";
+            int inputtxtlength = inputtxt.Length;
+            if (inputtxtlength > 0) {
+                //Cycle for the entire string that select a single char at time
+                for (int i = 0; i < inputtxtlength; i++) {
+                    char x = inputtxt[i];
+                    if (alphabetl.Contains(x) || alphabetu.Contains(x)) {
+                    //Cycles that detect char number and shift it by 13.
+                        for (int c = 0; c < alphabetlen; c++) {
+                            //Lowercase chars
+                            if (x == alphabetl[c]) {
+                                int cshift = c + shiftnum;
+                                //If shift becames bigger than alphabet it substract alphabet lenght, otherwise it crash.
+                                if (cshift >= alphabetlen) {
+                                    cshift -= alphabetlen;
+                                }
+                                outputtext += alphabetl[cshift];
+                                break;
+                            }
+                            //Uppercase chars
+                            if (x == alphabetu[c]) {
+                                int cshift = c + shiftnum;
+                                if (cshift >= alphabetlen) {
+                                    cshift -= alphabetlen;
+                                }
+                                outputtext += alphabetu[cshift];
+                                break;
+                            }
+                        }
+                    }
+                    //For ANY other character
+                    else {
+                        outputtext += x;
+                    }
+                }
+            }
+            return outputtext;
+        }
+
+        private void rottext() {
+            textBox_output.Text = shifter(textBox_input.Text);
+        }
+        private void textBox_input_TextChanged(object sender, EventArgs e) {
+            rottext();
+        }
+
+    }
+}
